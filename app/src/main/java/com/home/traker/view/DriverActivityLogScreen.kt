4d9118aae81229abfe8d16c5dde5393c9463cd81
vikash3891 <shukla.vikash38@gmail.com>
2020-06-28 02:01:49 +0530

    Location update on Punch In

--- app/src/main/java/com/home/traker/view/DriverActivityLogScreen.kt
+++ app/src/main/java/com/home/traker/view/DriverActivityLogScreen.kt
@@ -1,21 +1,24 @@
 package com.home.traker.view
 
-import android.app.job.JobInfo
-import android.app.job.JobScheduler
-import android.content.ComponentName
+import android.Manifest
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.view.animation.LinearInterpolator
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.*
 import com.home.traker.R
 import com.home.traker.api.ApiClient
 import com.home.traker.api.ApiInterface
@@ -23,13 +26,13 @@ import com.home.traker.api.RequestModel
 import com.home.traker.api.ResponseModelClasses
 import com.home.traker.base.BaseActivity
 import com.home.traker.databinding.ActivityDriverActivityLogBinding
-import com.home.traker.navigators.OnItemSelectionListener
 import com.home.traker.model.ListItemModel
-import com.home.traker.network.LocationTrackingService
+import com.home.traker.navigators.OnItemSelectionListener
 import com.home.traker.utils.AppPrefences
 import com.home.traker.utils.Constants
 import com.home.traker.utils.Utils
 import kotlinx.android.synthetic.main.activity_driver_activity_log.*
+import kotlinx.android.synthetic.main.item_dialog.view.*
 import kotlinx.android.synthetic.main.topbar_layout.view.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -40,6 +43,8 @@ import kotlin.collections.ArrayList
 
 class DriverActivityLogScreen : BaseActivity(),
     OnItemSelectionListener {
+    val PERMISSION_ID = 42
+
     override fun getPosition(position: Int, isForTeamA: Boolean) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -49,9 +54,12 @@ class DriverActivityLogScreen : BaseActivity(),
     }
 
     var foodsList = ArrayList<String>()
+    var driverLocation = ArrayList<String>()
     private lateinit var binding: ActivityDriverActivityLogBinding
     private lateinit var viewModel: ListItemModel
     private var isPunchedIn = false
+
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,6 +67,9 @@ class DriverActivityLogScreen : BaseActivity(),
         binding = DataBindingUtil.setContentView(this, R.layout.activity_driver_activity_log)
         topbar.imageSetting.background = resources.getDrawable(R.drawable.ic_account_white_24dp)
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        driverLocation.add("" + 0.0 + " - " + 0.0)
         viewModel = ListItemModel("", "", "", "", "", "", "", "", "", "")
         binding.viewmodel = viewModel
         try {
@@ -84,13 +95,16 @@ class DriverActivityLogScreen : BaseActivity(),
                 textLogout.setOnClickListener {
                     logoutAlertDialog()
                 }
+                showList.setOnClickListener {
+                    openDialog()
+                }
 
                 textPunchIn.setOnClickListener {
 
                     if (!isPunchedIn) {
                         punchInOutApi(isPunchedIn)
                         isPunchedIn = true
-                        Toast.makeText(this, "Punch IN", Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(this, "Punch IN", Toast.LENGTH_SHORT).show()
                         textPunchIn.alpha = 0.2f
                         textPunchIn.animate().apply {
                             interpolator = LinearInterpolator()
@@ -99,7 +113,7 @@ class DriverActivityLogScreen : BaseActivity(),
                             alpha(1f)
                             start()
                         }
-                        val jobScheduler =
+                        /*val jobScheduler =
                             getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
 
                         val jobInfo = JobInfo.Builder(
@@ -111,7 +125,9 @@ class DriverActivityLogScreen : BaseActivity(),
                         )
                             // only add if network access is required
                             .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY).build()
-                        jobScheduler.schedule(jobInfo)
+                        jobScheduler.schedule(jobInfo)*/
+                        startLoaction()
+                        startService(Intent(this, ForegroundService::class.java))
                     } else {
                         Toast.makeText(this, "Already Punch IN", Toast.LENGTH_SHORT).show()
                     }
@@ -122,7 +138,7 @@ class DriverActivityLogScreen : BaseActivity(),
                     if (isPunchedIn) {
                         punchInOutApi(isPunchedIn)
                         isPunchedIn = false
-                        Toast.makeText(this, "Punch OUT", Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(this, "Punch OUT", Toast.LENGTH_SHORT).show()
                         textPunchOut.alpha = 0.2f
                         textPunchOut.animate().apply {
                             interpolator = LinearInterpolator()
@@ -131,8 +147,10 @@ class DriverActivityLogScreen : BaseActivity(),
                             alpha(1f)
                             start()
                         }
-                        val stopIntent = Intent(this, LocationTrackingService::class.java)
-                        stopService(stopIntent)
+                        /*val stopIntent = Intent(this, LocationTrackingService::class.java)
+                        stopService(stopIntent)*/
+                        Handler().removeCallbacks(null)
+                        stopService(Intent(this, ForegroundService::class.java))
                     } else {
                         Toast.makeText(this, "Already Punch OUT", Toast.LENGTH_SHORT).show()
                     }
@@ -244,5 +262,150 @@ class DriverActivityLogScreen : BaseActivity(),
 
         alertDialog.show()
 
+    }
+
+    private fun startLoaction() {
+        Handler().postDelayed({
+            /* Create an Intent that will start the Menu-Activity. */
+            getLastLocation()
+        }, 3000)
+
+    }
+
+    private fun getLastLocation() {
+
+        try {
+            if (checkPermissions()) {
+                if (isLocationEnabled()) {
+
+                    mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                        var location: Location? = task.result
+                        if (location == null) {
+                            requestNewLocationData()
+                        } else {
+                            /*findViewById<TextView>(R.id.latTextView).text = location.latitude.toString()
+                            findViewById<TextView>(R.id.lonTextView).text =
+                                location.longitude.toString()
+                            loactiondab.New(location.latitude, location.longitude)*/
+
+                            Toast.makeText(
+                                this,
+                                "Location: " + location.latitude + " - " + location.longitude,
+                                Toast.LENGTH_LONG
+                            ).show()
+                            driverLocation.add("" + location.latitude + " - " + location.longitude)
+                            driverLocation.add("" + location.latitude + " - " + location.longitude)
+                            driverLocation.add("" + location.latitude + " - " + location.longitude)
+                            driverLocation.add("" + location.latitude + " - " + location.longitude)
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(intent)
+                }
+            } else {
+                requestPermissions()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun openDialog() {
+        try {/*alertDialog = AlertDialog.Builder(this)
+        val rowList: View = layoutInflater.inflate(R.layout.item_dialog, null)
+        val listView:RecyclerView = rowList.findViewById(R.id.listRecyc)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, driverLocation)
+        listView.adapter = adapter
+        adapter.notifyDataSetChanged()
+        alertDialog.setView(rowList)
+        dialog = alertDialog.create()
+        dialog.show()*/
+            val dialog = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.item_dialog, null)
+            dialog.setView(view)
+            var location = StringBuilder()
+            for (i in 0..driverLocation.size - 1) {
+                location!!.append("\n" + driverLocation[i])
+            }
+            view.listRecyc.text = location.toString()
+            dialog.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            Toast.makeText(
+                this@DriverActivityLogScreen,
+                "Location" + mLastLocation.latitude + "-" + mLastLocation.longitude,
+                Toast.LENGTH_LONG
+            ).show()
+            /*findViewById<TextView>(R.id.latTextView).text = mLastLocation.latitude.toString()
+            findViewById<TextView>(R.id.lonTextView).text = mLastLocation.longitude.toString()*/
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            PERMISSION_ID
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLastLocation()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/home/traker/view/ForegroundService.kt
@@ -0,0 +1,92 @@
+package com.home.traker.view
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.home.traker.R
+
+
+class ForegroundService : Service() {
+
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            ContextCompat.startForegroundService(
+                context,
+                Intent(context, ForegroundService::class.java)
+            )
+        }
+
+        @JvmStatic
+        fun stop(context: Context) {
+            context.stopService(Intent(context, ForegroundService::class.java))
+        }
+    }
+
+    // Foreground service notification =========
+
+    private val foregroundNotificationId: Int = (System.currentTimeMillis() % 10000).toInt()
+    private val foregroundNotification by lazy {
+
+        NotificationCompat.Builder(this, foregroundNotificationChannelId)
+            .setSmallIcon(R.drawable.traker_logo)
+            .setContentTitle("My notification")
+            .setContentText("User Punched-IN. Sharing location...")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("User Punched-IN. Sharing location...")
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setSound(null)
+            .build()
+    }
+    private val foregroundNotificationChannelName by lazy {
+        getString(R.string.app_name)
+    }
+    private val foregroundNotificationChannelDescription by lazy {
+        "User Punched-IN. Sharing location"
+    }
+    private val foregroundNotificationChannelId by lazy {
+        "ForegroundServiceSample.NotificationChannel".also {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).apply {
+                    if (getNotificationChannel(it) == null) {
+                        createNotificationChannel(
+                            NotificationChannel(
+                                it,
+                                foregroundNotificationChannelName,
+                                NotificationManager.IMPORTANCE_MIN
+                            ).also {
+                                it.description = foregroundNotificationChannelDescription
+                                it.lockscreenVisibility = NotificationCompat.VISIBILITY_PRIVATE
+                                it.vibrationPattern = null
+                                it.setSound(null, null)
+                                it.setShowBadge(false)
+                            })
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Lifecycle ===============================
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForeground(foregroundNotificationId, foregroundNotification)
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+}
